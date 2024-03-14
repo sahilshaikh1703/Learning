492d6dcc759898f6036cd9e0a015a3ad4cf6f477
sahilshaikh1703 <110923579+sahilshaikh1703@users.noreply.github.com>
2024-03-14 16:15:57 +0530

    Update ArmstrongNumberBetween1toN.java

--- Tap academy Learning/program practice/src/codingProgramms/ArmstrongNumberBetween1toN.java
+++ Tap academy Learning/program practice/src/codingProgramms/ArmstrongNumberBetween1toN.java
@@ -1,39 +1,39 @@
 package codingProgramms;
 
-public class ArmstrongNumberBetween1toN {
+import java.util.HashSet;
+import java.util.Set;
 
+public class ArmstrongNumberBetween1toN {
 
 	public static void main(String[] args) {
 		int lastNum = 1000;
-		int count = 0;
+  Set armstrongSet =  new HashSet();
+		
+		for (int num = 1; num <= lastNum; num++) {
+			
+			int count = 0;
 
+			int orginalNum = num;
+			String n = Integer.toString(num);
+			while (num != 0) {
+				num = num / 10;
+				++count;
+			}
+			num = orginalNum;
+			int sum = 0;
+			for (int i = 0; i < count; i++) {
+				int digit = Character.getNumericValue(n.charAt(i)); // Convert character to integer
 
-	for(int num = 1 ; num<= lastNum ; num ++ ) {
-		int orginalNum = num;
-		String n = Integer.toString(num);
-		while (num != 0) {
-			num = num / 10;
-			++count;
-		}
-		System.out.println(orginalNum);
-		System.out.println("Number of digits: " + count);
-		num = orginalNum;
-		int sum = 0;
-		for (int i = 0; i < count; i++) {
-			int digit = Character.getNumericValue(n.charAt(i)); // Convert character to integer
+				sum = sum + (int) Math.pow(digit, count);
+			}
 
-			sum = sum + (int) Math.pow(digit, count);
-		}
+			if (sum == num)
 
-		System.out.println("The Total Sum is : " + sum);
-		if (sum == num)
-
-		{
-			System.out.println("It is an Armstrong Number ");
-		} else {
-			System.out.println("It is not an Armstrong Number ");
+			{
+				armstrongSet.add(num);
+			} 
 
 		}
-	}
+		System.out.println("The Armstrong Numbers between 1 & "+lastNum + " are: " +   armstrongSet);
 	}
 }
